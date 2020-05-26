d855fa0f79dff8de4eefca8f354bd2cb64455a44
joaodaarte <49598223+joaodaarte@users.noreply.github.com>
2020-05-26 18:20:22 -0300

    nenhum echo

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 
-echo "Hello, World!";
-echo "Mais um textinho: abcdefg";
-echo "Outro texto: 1234";
-echo "Agora uma novidade: texto local";
-echo "Echo 5";
-echo "Echo 6 e 7";
-echo "Echo 8";
+echo "Nenhum echo aqui";
 
 ?>
